027e71ec9c023d4dd8a0c4a00126bc81e5e1a4bd
Tausif Islam Sheik <mr.tausif904@gmail.com>
2024-12-03 22:37:22 +0600

    some information edited

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,6 @@
 const allSeat = document.querySelectorAll('.seat');
 for(const seat of allSeat){
     seat.addEventListener('click', function(e){
-        console.log(e.target);
-        // e.target = document.createElement()
+        e.target
     })
 }
