7b71e988a36d3fbcdc87ea7776a3112941860528
Tausif Islam Sheik <mr.tausif904@gmail.com>
2024-12-03 19:47:04 +0600

    selected seat added

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,2 +1,7 @@
-const seats = document.getElementsByClassName('seat');
-seats.a
+const allSeat = document.querySelectorAll('.seat');
+for(const seat of allSeat){
+    seat.addEventListener('click', function(e){
+        console.log(e.target);
+        // e.target = document.createElement()
+    })
+}
